cce0cdcaa9728595a7fc2f0a43a37f35deafae09
N-eeraj <neeraj7rajeena@gmail.com>
2025-04-27 20:01:40 +0530

    Updated shadcn & tailwind to latest version

--- src/types/github.d.ts
+++ src/types/github.d.ts
@@ -2,9 +2,9 @@ import { z } from "zod"
 import githubRepoDetailsSchema from "@schema/github/repoDetails"
 
 export interface GithubLanguage {
-  language: string;
-  hexCode: string;
-  percentage: number;
+  language: string
+  hexCode: string
+  percentage: number
 }
 
 export type GithubLanguageResponse = Array<GithubLanguage>
