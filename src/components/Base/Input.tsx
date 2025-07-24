0a58e9970ed552b8d725076709cdab0215ef7690
N-eeraj <neeraj7rajeena@gmail.com>
2025-07-25 02:15:08 +0530

    added password text toggle

--- src/components/Base/Input.tsx
+++ src/components/Base/Input.tsx
@@ -2,25 +2,44 @@
 
 import {
   useId,
+  useState,
 } from "react"
 import ErrorMessage from "@components/Base/ErrorMessage"
 import { Input } from "@shadcn/input"
 import { Label } from "@shadcn/label"
+import { Button } from "@shadcn/button"
+import { Icon } from "@iconify/react"
 import type { InputProps } from "@customTypes/ui/input"
 
-function BaseInput({ label, error, ...props }: InputProps) {
+function BaseInput({ label, type, error, ...props }: InputProps) {
   const id = useId()
+  const [showPassword, setShowPassword] = useState(false)
+  const currentType = showPassword ? "text" : type
+
+  const togglePassword = () => setShowPassword(showPassword => !showPassword)
 
   return (
-    <div className="grid gap-2">
+    <div className="relative grid gap-2">
       {label && (
         <Label htmlFor={id}>
           {label}
         </Label>
       )}
+
       <Input
+        {...props}
         id={id}
-        {...props} />
+        type={currentType} />
+
+      {type === "password" && (
+        <Button
+          variant="ghost"
+          type="button"
+          className="absolute top-5.5 right-0"
+          onClick={togglePassword}>
+          <Icon icon={showPassword ? "lucide:eye" : "lucide:eye-off"} />
+        </Button>
+      )}
 
       <ErrorMessage error={error} />
     </div>
