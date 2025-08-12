ae4a240853e4f12852b2a5a2f6e470071419303b
N-eeraj <neeraj7rajeena@gmail.com>
2025-08-12 09:52:28 +0530

    Refactor RateLimiter

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,39 +2,17 @@ import {
   NextResponse,
   type NextRequest,
 } from "next/server"
+import RateLimiter from "@utils/RateLimiter"
 
-import normalizeIp from "@utils/normalizeIp"
-import { sendErrorResponse } from "@server/lib/responseHandlers"
-
-const rateLimiter = new Map()
-const REQUEST_LIMIT = 60
-const RATE_LIMITER_WINDOW = 60_000 // 1 minute
+const rateLimiter = new RateLimiter(3)
 
 export default async function middleware(request: NextRequest) {
   if (request.url.includes("too-many-requests")) {
     return NextResponse.next()
   }
 
-  const ip = normalizeIp(request)
-  let trackedRequests = rateLimiter.get(ip)
-  const now = Date.now()
-  if (trackedRequests) {
-    rateLimiter.set(ip, trackedRequests.filter((trackedTime: number) => trackedTime > now - RATE_LIMITER_WINDOW))
-    trackedRequests = rateLimiter.get(ip)
-    if (trackedRequests.length > REQUEST_LIMIT) {
-      if (request.nextUrl.pathname.startsWith("/api")) {
-        return sendErrorResponse({
-          status: 429,
-          message: "Too Many Requests"
-        })
-      } else {
-        return NextResponse.redirect(new URL("/too-many-requests", request.url))
-      }
-    }
-    trackedRequests.push(now)
-  } else {
-    rateLimiter.set(ip, [now])
-  }
+  const limitExceeded = rateLimiter.checkRateLimit(request)
+  if (limitExceeded) return limitExceeded
 
   return NextResponse.next()
 }
@@ -50,4 +28,3 @@ export const config = {
     "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\.\\w+$).*)",
   ],
 }
-
