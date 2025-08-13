2a4c7fe773e5040bec9424c89a78b28acac9694e
Neeraj <70063961+N-eeraj@users.noreply.github.com>
2025-08-13 10:24:53 +0530

    Logins redirect to is lost when switched sign up (#6)
    * fix: add query params on auth form footer links

--- src/app/(auth)/components/Form/Footer.tsx
+++ src/app/(auth)/components/Form/Footer.tsx
@@ -1,10 +1,17 @@
+"use client"
+
 import Link from "next/link"
+import { useSearchParams } from "next/navigation"
 import { Button } from "@shadcn/button"
 import { CardFooter } from "@shadcn/card"
 import type { AuthFormFooterProps } from "@auth/types/form"
 
 function AuthFormFooter({ footerText, segue }: AuthFormFooterProps) {
+  const searchParams = useSearchParams()
   if (!(footerText || segue)) return
+
+  const queryParams = Object.fromEntries(searchParams.entries())
+  const segueLink = `${(segue?.link ?? "")}?${new URLSearchParams(queryParams).toString()}`
 
   return (
     <CardFooter className="justify-center gap-x-1 mt-2">
@@ -15,7 +22,7 @@ function AuthFormFooter({ footerText, segue }: AuthFormFooterProps) {
       )}
       {segue && (
         <Link
-          href={segue.link}
+          href={segueLink}
           scroll={false}
           replace={segue.replace}>
           <Button
